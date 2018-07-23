a44a044708aa2012c9c12c1e0d11fb713cc7cd41
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-07-23 19:10:46 +0200

    Adaptative design with grids

---
+++ src/Adaptative.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './Adaptative.scss';
+
+
+// See http://tobiasahlin.com/spinkit/
+export default class Adaptative extends React.PureComponent {
+
+  render() {
+    return (
+      <div className="adaptative">
+        <div className="adaptative__item">
+          Proposal 1
+        </div>
+        <div className="adaptative__item">
+          Proposal 2
+        </div>
+        <div className="adaptative__item">
+          Proposal 3
+        </div>
+        <div className="adaptative__item">
+          Proposal 4
+        </div>
+      </div>
+    );
+  }
+
+}
---
+++ src/Adaptative.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Adaptative from './Adaptative';
+
+
+storiesOf('Adaptative', module)
+  .add('main', () => {
+    return (
+      <Adaptative />
+    );
+  });
