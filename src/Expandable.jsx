fdbbcc2e2ece72d2667e9d2ef922ca22f82b1561
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-12 11:38:51 +0200

    Animate function in its own module

--- src/Expandable.jsx
+++ src/Expandable.jsx
@@ -1,6 +1,8 @@
 import React, { createRef } from 'react';
 import PropTypes from 'prop-types';
 
+import animate from './animate';
+
 import './Expandable.scss';
 
 
@@ -91,30 +93,5 @@ export default class Expandable extends React.PureComponent {
     );
   }
 
-}
-
-
-async function animate(duration, callback) {
-  return new Promise(resolve => {
-    let startTime = null;
-
-    const step = (timestamp) => {
-      if (startTime === null) {
-        startTime = timestamp;
-      }
-
-      const maxProgress = 1.0;
-      const progress = Math.min((timestamp - startTime) / duration, maxProgress);
-      callback(progress);
-
-      if (progress < maxProgress) {
-        requestAnimationFrame(step);
-      } else {
-        resolve();
-      }
-    }
-
-    requestAnimationFrame(step);
-  });
 }
 
---
+++ src/animate.js
@@ -0,0 +1,23 @@
+export default async function animate(duration, callback) {
+  return new Promise(resolve => {
+    let startTime = null;
+
+    const step = (timestamp) => {
+      if (startTime === null) {
+        startTime = timestamp;
+      }
+
+      const maxProgress = 1.0;
+      const progress = Math.min((timestamp - startTime) / duration, maxProgress);
+      callback(progress);
+
+      if (progress < maxProgress) {
+        requestAnimationFrame(step);
+      } else {
+        resolve();
+      }
+    }
+
+    requestAnimationFrame(step);
+  });
+}
