dd7f1c5665af84b0ebbb7b483c1b906526acee4c
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-12 12:05:36 +0200

    Removed useless intl

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,22 +1,6 @@
 import 'babel-polyfill';
 
 import { configure, addDecorator } from '@storybook/react';
-import { setIntlConfig, withIntl } from 'storybook-addon-intl';
-import { addLocaleData } from 'react-intl';
-
-import enLocaleData from 'react-intl/locale-data/en';
-import frLocaleData from 'react-intl/locale-data/fr';
-
-addLocaleData(enLocaleData);
-addLocaleData(frLocaleData);
-
-setIntlConfig({
-  locales: ['en', 'fr'],
-  defaultLocale: 'fr',
-  getMessages: () => null,
-});
-
-addDecorator(withIntl);
 
 
 // automatically import all files ending in *.stories.js or *.stories.jsx
