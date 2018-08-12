5dd92c79d8aeff82e494ebf3eb250786fd5535d6
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-12 12:57:49 +0200

    Storyshot tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rootDir: './src',
+  testURL: 'http://localhost/',
+  moduleNameMapper: {
+    '\\.(scss|css)$': '<rootDir>/__mocks__/styleMock.js'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!lodash-es)/',
+  ],
+};
---
+++ src/_storyshots.test.js
@@ -0,0 +1,8 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+// Each story will generate a snapshot test
+
+initStoryshots({
+  storyKindRegex:/^(?!.*Expandable).*/,
+  storyNameRegex:/(?!.*Expandable).*/
+});
