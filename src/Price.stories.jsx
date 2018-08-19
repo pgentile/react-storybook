50483637bceca68ab3b17e24f5f6e3445522bf73
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-19 09:03:43 +0200

    Stories pour les prix

---
+++ src/Price.stories.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Price from "./Price";
+
+const values = [0, 12, 21.9, 35.2, 46.5, -10.9, -45.2];
+
+const stories = storiesOf("Price", module);
+
+values.forEach(value => {
+  stories.add(`Prix - Valeur de ${value} €`, () => {
+    return <Price price={{ value, currency: "€" }} />;
+  });
+});
+
+stories.add(`Prix - En pounds`, () => {
+  return <Price price={{ value: 135.9, currency: "£" }} />;
+});
+
+stories.add(`Prix - En franc suisse`, () => {
+  return <Price price={{ value: 135.9, currency: "CHF" }} />;
+});
