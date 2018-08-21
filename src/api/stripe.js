c9d783205311dec65c5ca079e79b5526ac21aacb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-21 20:25:39 +0200

    More Stripe API

--- src/api/stripe.js
+++ src/api/stripe.js
@@ -21,3 +21,60 @@ export async function createToken({ cardNumber, expirationDate, cvv }) {
 
   return await response.json();
 }
+
+export async function charge({ token, amount, currency, description }) {
+  const body = new URLSearchParams();
+  body.append("amount", Math.trunc(amount * 100));
+  body.append("currency", currency);
+  body.append("source", token);
+  body.append("description", description);
+  body.append("capture", "true");
+
+  const response = await fetch("https://api.stripe.com/v1/charges", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      Accept: "application/json"
+    },
+    body
+  });
+
+  return await response.json();
+}
+
+export async function createCard({ token }) {
+  const body = new URLSearchParams();
+  body.append("type", "card");
+  body.append("token", token);
+
+  const response = await fetch("https://api.stripe.com/v1/sources", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      Accept: "application/json"
+    },
+    body
+  });
+
+  return await response.json();
+}
+
+export async function create3DSSource({ cardId, amount, currency }) {
+  const body = new URLSearchParams();
+  body.append("type", "three_d_secure");
+  body.append("amount", Math.trunc(amount * 100));
+  body.append("currency", currency);
+  body.append("redirect[return_url]", window.location);
+  body.append("three_d_secure[card]", cardId);
+
+  const response = await fetch("https://api.stripe.com/v1/sources", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      Accept: "application/json"
+    },
+    body
+  });
+
+  return await response.json();
+}
