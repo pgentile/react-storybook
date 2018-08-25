8414a1e253185704f915de769f9c56348160c25a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-08-25 18:04:57 +0200

    Better checkable input

--- src/forms/CheckableImageInput.jsx
+++ src/forms/CheckableImageInput.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import PropTypes from "prop-types";
 
 import bemModifiers from "../utils/bemModifiers";
@@ -7,28 +7,54 @@ import "./CheckableImageInput.scss";
 
 export default class CheckableImageInput extends React.PureComponent {
   static propTypes = {
+    className: PropTypes.string,
     children: PropTypes.node.isRequired,
     checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    readOnly: PropTypes.bool,
     type: PropTypes.oneOf(["checkbox", "radio"]),
     name: PropTypes.string,
+    value: PropTypes.string,
     onChange: PropTypes.func
   };
 
   static defaultProps = {
+    className: "",
     checked: false,
     type: "radio"
   };
 
+  inputRef = createRef();
+
+  onClick = () => {
+    if (this.inputRef && this.inputRef.current) {
+      this.inputRef.current.click();
+    }
+  };
+
   render() {
-    const { children, checked, type, name, onChange } = this.props;
+    const { className, children, checked, type, name, value, disabled, readOnly, onChange } = this.props;
 
     const realClassName = bemModifiers("checkable-image-input", {
-      checked
+      checked,
+      disabled,
+      "read-only": readOnly
     });
 
     return (
-      <span className={realClassName}>
-        <input className="checkable-image-input__input" type={type} name={name} checked={checked} onChange={onChange} />
+      <span className={realClassName + " " + className} onClick={this.onClick} tabIndex={0}>
+        <input
+          className="checkable-image-input__input"
+          tabIndex={-1}
+          ref={this.inputRef}
+          type={type}
+          name={name}
+          value={value}
+          checked={checked}
+          disabled={disabled}
+          readOnly={readOnly}
+          onChange={onChange}
+        />
         {children}
       </span>
     );
