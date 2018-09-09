6becbacf068566631ac4e6b28bcca85f22ee674d
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-09-09 10:03:12 +0200

    Other JSON schema forms

--- src/jsonschema/JsonSchemaForm.stories.jsx
+++ src/jsonschema/JsonSchemaForm.stories.jsx
@@ -4,6 +4,8 @@ import { action } from "@storybook/addon-actions";
 
 import SearchOutwardForm from "./SearchOutwardForm";
 import SearchInwardForm from "./SearchInwardForm";
+import UpdateOwnerForm from "./UpdateOwnerForm";
+import UpdatePassengersForm from "./UpdatePassengersForm";
 
 const actions = {
   onSubmit: action("submit"),
@@ -16,4 +18,10 @@ storiesOf("JsonSchemaForm", module)
   })
   .add("SearchInwardForm", () => {
     return <SearchInwardForm {...actions} />;
+  })
+  .add("UpdateOwnerForm", () => {
+    return <UpdateOwnerForm {...actions} />;
+  })
+  .add("UpdatePassengersForm", () => {
+    return <UpdatePassengersForm {...actions} />;
   });
---
+++ src/jsonschema/UpdatePassengersForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Form from "react-jsonschema-form";
+
+import schema from "./schemas/updatePassengers.request.json";
+
+export default class UpdatePassengersForm extends React.PureComponent {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { onSubmit } = this.props;
+
+    const formData = {};
+
+    return <Form schema={schema} formData={formData} onSubmit={onSubmit} />;
+  }
+}
