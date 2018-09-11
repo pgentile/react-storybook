b6a372b24c059b3a9173857f90c5f69497c5944c
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-09-11 21:27:50 +0200

    Event handler

--- src/forms/NumberInput.stories.jsx
+++ src/forms/NumberInput.stories.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
 import NumberInput from "./NumberInput";
 
 storiesOf("Forms / NumberInput", module).add("main", () => {
-  return <NumberInput />;
+  return <NumberInput onChange={action("change")} />;
 });
