6c1da90dc98333c9afb4375ddb98a50c5b28481b
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-03-30 13:00:36 +0100

    Use buttons from lib

--- src/ExchangeRate.stories.jsx
+++ src/ExchangeRate.stories.jsx
@@ -1,24 +1,23 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import ExchangeRate, { ExchangeRateConverter, ExchangeRateSwitcher } from "./ExchangeRate";
 import Price from "./Price";
+import ButtonGroup, { ButtonInGroup } from "./buttons/ButtonGroup";
 
 storiesOf("ExchangeRate", module).add("main", () => {
   const price = { value: 10, currency: "EUR" };
   return (
     <ExchangeRate>
-      <p>
-        <ExchangeRateSwitcher>
-          {({ setCurrency }) => (
-            <Fragment>
-              <button onClick={() => setCurrency("CHF")}>To CHF</button>{" "}
-              <button onClick={() => setCurrency("EUR")}>To EUR</button>{" "}
-              <button onClick={() => setCurrency("GBP")}>To GBP</button>
-            </Fragment>
-          )}
-        </ExchangeRateSwitcher>
-      </p>
+      <ExchangeRateSwitcher>
+        {({ setCurrency }) => (
+          <ButtonGroup>
+            <ButtonInGroup onClick={() => setCurrency("CHF")}>To CHF</ButtonInGroup>
+            <ButtonInGroup onClick={() => setCurrency("EUR")}>To EUR</ButtonInGroup>
+            <ButtonInGroup onClick={() => setCurrency("GBP")}>To GBP</ButtonInGroup>
+          </ButtonGroup>
+        )}
+      </ExchangeRateSwitcher>
       <p>
         <Price price={price} />
       </p>
