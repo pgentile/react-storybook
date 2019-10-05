e3b67f28ae847723bf925a15f02922222a181485
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-10-05 07:51:17 +0400

    Fixed encryption error

--- src/Encryption.stories.jsx
+++ src/Encryption.stories.jsx
@@ -5,6 +5,6 @@ import { storiesOf } from "@storybook/react";
 import encryptPayload from "./encryptPayload";
 
 storiesOf("Encryption", module).add("test", () => {
-  encryptPayload("This is my data");
+  encryptPayload("This is my data").catch(e => console.error("Failed to encrypt payload: %s", e));
   return <div style={{ color: "red", fontSize: "3em", fontWeight: "bold" }}>Please look at your console</div>;
 });
