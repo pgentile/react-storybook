2b64ee2d64373127c7e0e4730213a856c6d83807
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-12-28 17:27:14 +0100

    Night mode with hooks

--- src/NightMode.jsx
+++ src/NightMode.jsx
@@ -1,46 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import bemModifiers from "./utils/bemModifiers";
 
 import "./NightMode.scss";
 
-export default class NightMode extends React.PureComponent {
-  static propTypes = {
-    children: PropTypes.node
+export default function NightMode({ children }) {
+  const [enabled, setEnabled] = useState(false);
+
+  const className = bemModifiers("night-mode", {
+    enabled
+  });
+
+  const onSwitchClick = () => {
+    setEnabled(!enabled);
   };
 
-  static defaultProps = {};
-
-  state = {
-    enabled: false
-  };
-
-  switch = () => {
-    this.setState(oldState => {
-      return {
-        enabled: !oldState.enabled
-      };
-    });
-  };
-
-  render() {
-    const { children } = this.props;
-    const { enabled } = this.state;
-
-    const className = bemModifiers("night-mode", {
-      enabled
-    });
-
-    return (
-      <div className={className}>
-        <p className="night-mode__controls">
-          <button className="night-mode__switcher" onClick={this.switch}>
-            {enabled ? "Disable" : "Enable"} night mode
-          </button>
-        </p>
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div className={className}>
+      <p className="night-mode__controls">
+        <button className="night-mode__switcher" onClick={onSwitchClick}>
+          {enabled ? "Disable" : "Enable"} night mode
+        </button>
+      </p>
+      {children}
+    </div>
+  );
 }
+
+NightMode.propTypes = {
+  children: PropTypes.node
+};
