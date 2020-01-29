2b17e6fb1feebaa3c462e65a96f830d9390859e3
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-01-29 22:11:44 +0100

    Web Share component

---
+++ src/samples/WebShareSample.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function WebShareSample({ url, text, title }) {
+  const onShareClick = async () => {
+    await navigator.share({
+      url,
+      text,
+      title
+    });
+  };
+  return (
+    <div className="web-share">
+      <button onClick={onShareClick}>Partager</button>
+    </div>
+  );
+}
+
+WebShareSample.propTypes = {
+  url: PropTypes.string,
+  text: PropTypes.string,
+  title: PropTypes.string
+};
---
+++ src/samples/WebShareSample.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import WebShareSample from "./WebShareSample";
+
+export default {
+  title: "Samples | WebShareSample",
+  component: WebShareSample
+};
+
+export const main = () => <WebShareSample url={document.URL} title="WebShare API sample" />;
