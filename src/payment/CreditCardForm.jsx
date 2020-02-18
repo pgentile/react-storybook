d8ab85c656df01343128ebe64b3caad221c22414
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-02-18 22:45:47 +0100

    Removed Formik form

--- src/payment/CreditCardForm.jsx
+++ src/payment/CreditCardForm.jsx
@@ -1,186 +1,164 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withFormik, Formik } from "formik";
+import { Form, useField, useFormState } from "react-final-form";
+import createDecorator from "final-form-focus";
 import cardValidator from "card-validator";
 import creditCardType from "credit-card-type";
 
 import FieldContainer from "../forms/FieldContainer";
 import InputField from "../forms/InputField";
 import NumberInput from "../forms/NumberInput";
-import DateInput from "../forms/DateInput";
 import Button from "../buttons/Button";
 import Price from "../Price";
+import Card from "../Card";
 
 import "./CreditCardForm.scss";
 
-class CreditCardForm extends React.PureComponent {
-  static propTypes = {
-    ...Formik.propTypes,
-    className: PropTypes.string,
-    totalPrice: PropTypes.shape({
-      value: PropTypes.number.isRequired,
-      currency: PropTypes.string.isRequired
-    }).isRequired,
-    onPay: PropTypes.func.isRequired
+const focusOnErrors = createDecorator();
+
+export default function CreditCardForm({ className = "", totalPrice, onPay }) {
+  const onSubmit = async values => {
+    await onPay(values);
   };
 
-  static defaultProps = {
-    className: ""
-  };
-
-  render() {
-    const {
-      className,
-      totalPrice,
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      setFieldValue,
-      setFieldTouched,
-      isSubmitting
-    } = this.props;
-    const disableForm = isSubmitting;
-
-    const cards = creditCardType(values.cardNumber);
-    const isMaestro = cards && cards.length === 1 && cards[0].type === "maestro";
-    const cvvHelpMessage = isMaestro
-      ? "Certaines cartes Maestro ne possèdent pas de code de sécurité. Si aucun code n'est présent, ne renseignez pas ce champ"
-      : null;
-
-    return (
-      <form className={`credit-card-form ${className}`} onSubmit={handleSubmit}>
-        <FieldContainer
-          label="Numéro de carte"
-          className="credit-card-form__card-number"
-          disabled={disableForm}
-          errorMessage={touched.cardNumber && errors.cardNumber}
-        >
-          {props => (
-            <InputField
-              as={NumberInput}
-              {...props}
-              name="cardNumber"
-              autoComplete="cc-number"
-              maxLength={19}
-              value={values.cardNumber}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          )}
-        </FieldContainer>
-
-        <FieldContainer
-          label="Date d'expiration"
-          className="credit-card-form__expiration-date"
-          disabled={disableForm}
-          errorMessage={touched.expirationDate && errors.expirationDate}
-        >
-          {props => (
-            <DateInput
-              {...props}
-              mode="year-month"
-              smallYear
-              name="expirationDate"
-              value={values.expirationDate}
-              autoComplete={{ month: "cc-exp-month", year: "cc-exp-year" }}
-              onChange={value => setFieldValue("expirationDate", value)}
-              onBlur={() => setFieldTouched("expirationDate", true)}
-            />
-          )}
-        </FieldContainer>
-
-        <FieldContainer
-          label="Code de sécurité"
-          className="credit-card-form__cvv"
-          disabled={disableForm}
-          errorMessage={touched.cvv && errors.cvv}
-          helpMessage={cvvHelpMessage}
-          optional={isMaestro}
-        >
-          {props => (
-            <InputField
-              as={NumberInput}
-              {...props}
-              name="cvv"
-              autoComplete="cc-csc"
-              maxLength={4}
-              value={values.cvv}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          )}
-        </FieldContainer>
-
-        <div className="credit-card-form__button">
-          <Button size="large" type="submit" disabled={disableForm}>
-            Payer&nbsp;
-            <Price noColor price={totalPrice} />
-          </Button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <Form
+      initialValues={{
+        cardNumber: "",
+        expirationDate: "",
+        cvv: ""
+      }}
+      decorators={[focusOnErrors]}
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form className={`credit-card-form ${className}`} onSubmit={handleSubmit}>
+          <InternalCreditCardForm totalPrice={totalPrice} />
+        </form>
+      )}
+    ></Form>
+  );
 }
 
-const acceptedTypes = new Set(["visa", "mastercard", "american-express", "maestro"]);
+CreditCardForm.propTypes = {
+  className: PropTypes.string,
+  totalPrice: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired
+  }).isRequired,
+  onPay: PropTypes.func.isRequired
+};
 
-export default withFormik({
-  mapPropsToValues: () => ({
-    cardNumber: "",
-    cvv: "",
-    expirationDate: "",
-    name: ""
-  }),
-  validate: values => {
-    const errors = {};
+function InternalCreditCardForm({ totalPrice }) {
+  const { submitting, hasSubmitErrors, submitError } = useFormState();
 
-    const cardValidation = cardValidator.number(values.cardNumber);
-    const card = (cardValidation && cardValidation.card) || { type: null };
+  const cardNumber = useField("cardNumber", {
+    validate: value => {
+      const cardValidation = cardValidator.number(value);
+      if (!cardValidation.isValid) {
+        return "Numéro de carte invalide";
+      }
 
-    if (cardValidation === null || !cardValidation.isValid) {
-      errors.cardNumber = "Numéro de carte invalide";
-    } else {
-      const { type, niceType } = card;
+      const { type, niceType } = cardValidation.card;
       if (type) {
+        const acceptedTypes = new Set(["visa", "mastercard", "american-express", "maestro"]);
         if (!acceptedTypes.has(type)) {
-          errors.cardNumber = `Les cartes bancaires ${niceType} ne sont pas supportées`;
+          return `Les cartes bancaires ${niceType} ne sont pas supportées`;
         }
       }
-    }
+    },
+    validateFields: ["cvv"]
+  });
 
-    if (values.cvv) {
-      const cvvLength = cardValidation && cardValidation.card && cardValidation.card.code.size;
-      if (cvvLength && cvvLength !== values.cvv.length) {
-        errors.cvv = `Le code de sécurité doit faire ${cvvLength} chiffres`;
+  const expirationDate = useField("expirationDate", {
+    validate: value => {
+      const [year, month] = value.split("-");
+      if (!cardValidator.expirationDate({ year, month }).isValid) {
+        return "Date d'expiration invalide";
       }
-    } else {
-      const { type } = card;
-      if (type !== "maestro") {
-        errors.cvv = "Le code de sécurité est requis";
+    },
+    validateFields: []
+  });
+
+  const cvv = useField("cvv", {
+    validate: (value, allValues) => {
+      const { cardNumber } = allValues;
+      const cardValidation = cardValidator.number(cardNumber);
+
+      if (value) {
+        const cvvLength = cardValidation?.card?.code?.size;
+        if (cvvLength && cvvLength !== value.length) {
+          return `Le code de sécurité doit faire ${cvvLength} chiffres`;
+        }
+      } else {
+        const type = cardValidation?.card?.type;
+        if (type !== "maestro") {
+          return "Le code de sécurité est requis";
+        }
       }
-    }
+    },
+    validateFields: ["cardNumber"]
+  });
 
-    const [year, month] = values.expirationDate.split("-");
-    if (!cardValidator.expirationDate({ year, month }).isValid) {
-      errors.expirationDate = "Date d'expiration invalide";
-    }
+  const cards = creditCardType(cardNumber.input.value);
+  const isMaestro = cards && cards.length === 1 && cards[0].type === "maestro";
+  const cvvHelpMessage = isMaestro
+    ? "Certaines cartes Maestro ne possèdent pas de code de sécurité. Si aucun code n'est présent, ne renseignez pas ce champ"
+    : null;
 
-    return errors;
-  },
-  handleSubmit: async (values, { props, setSubmitting, setErrors }) => {
-    try {
-      await props.onPay(values);
-    } catch (e) {
-      console.error("Failed to submit", e);
+  return (
+    <>
+      {hasSubmitErrors && (
+        <Card layer="raised" className="credit-card-form__error">
+          {submitError}
+        </Card>
+      )}
 
-      if (e.details && e.details.type === "VALIDATION") {
-        setErrors(e.details.errors || {});
-      }
-    } finally {
-      setSubmitting(false);
-    }
-  }
-})(CreditCardForm);
+      <FieldContainer
+        label="Numéro de carte"
+        className="credit-card-form__card-number"
+        disabled={submitting}
+        errorMessage={cardNumber.meta.touched && cardNumber.meta.error}
+      >
+        {props => (
+          <InputField as={NumberInput} {...cardNumber.input} {...props} autoComplete="cc-number" maxLength={19} />
+        )}
+      </FieldContainer>
+
+      <FieldContainer
+        label="Date d'expiration"
+        className="credit-card-form__expiration-date"
+        disabled={submitting}
+        errorMessage={expirationDate.meta.touched && expirationDate.meta.error}
+      >
+        {props => <InputField {...expirationDate.input} {...props} autoComplete="cc-exp" maxLength={5} />}
+      </FieldContainer>
+
+      <FieldContainer
+        label="Code de sécurité"
+        className="credit-card-form__cvv"
+        disabled={submitting}
+        errorMessage={cvv.meta.touched && cvv.meta.error}
+        helpMessage={cvvHelpMessage}
+        optional={isMaestro}
+      >
+        {props => (
+          <InputField as={NumberInput} {...cvv.input} {...props} name="cvv" autoComplete="cc-csc" maxLength={4} />
+        )}
+      </FieldContainer>
+
+      <div className="credit-card-form__button">
+        <Button size="large" type="submit" disabled={submitting}>
+          Payer&nbsp;
+          <Price noColor price={totalPrice} />
+        </Button>
+      </div>
+    </>
+  );
+}
+
+InternalCreditCardForm.propTypes = {
+  totalPrice: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired
+  }).isRequired
+};
