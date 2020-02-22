d313f3fc94762242ee7be8ce5668577db50744fd
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-02-22 09:42:07 +0100

    final-form for RegistredCardCvvForm

--- src/payment/RegistredCardCvvForm.jsx
+++ src/payment/RegistredCardCvvForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { getTypeInfo } from "credit-card-type";
-import { withFormik, Formik } from "formik";
+import { Form, useField, useFormState, useForm } from "react-final-form";
+import createDecorator from "final-form-focus";
 
 import FieldContainer from "../forms/FieldContainer";
 import InputField from "../forms/InputField";
@@ -11,113 +12,128 @@ import Price from "../Price";
 
 import "./RegistredCardCvvForm.scss";
 
-class RegistredCardCvvForm extends React.PureComponent {
-  static propTypes = {
-    ...Formik.propTypes,
-    brand: PropTypes.string.isRequired,
-    totalPrice: PropTypes.shape({
-      value: PropTypes.number.isRequired,
-      currency: PropTypes.string.isRequired
-    }).isRequired,
-    disabled: PropTypes.bool,
-    onCancel: PropTypes.func.isRequired,
-    onUseCard: PropTypes.func.isRequired
+const focusOnErrors = createDecorator();
+
+export default function RegistredCardCvvForm({ brand, totalPrice, disabled, onCancel, onUseCard }) {
+  const onSubmit = async values => {
+    await onUseCard(values);
   };
 
-  onCancel = () => {
-    this.props.onCancel();
-  };
-
-  render() {
-    const {
-      brand,
-      totalPrice,
-      disabled,
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      handleReset,
-      isSubmitting
-    } = this.props;
-    const disableForm = isSubmitting || disabled;
-
-    const cardBrandInfo = getTypeInfo(brand);
-    const cvvLength = cardBrandInfo.code.size;
-    const isMaestro = brand === "maestro";
-    const cvvHelpMessage = isMaestro
-      ? "Certaines cartes Maestro ne possèdent pas de code de sécurité. Si aucun code n'est présent, ne renseignez pas ce champ"
-      : null;
-
-    return (
-      <form className="registred-card-cvv-form" onSubmit={handleSubmit} onReset={handleReset}>
-        <FieldContainer
-          className="registred-card-cvv-form__cvv"
-          label="Code de sécurité"
-          optional={isMaestro}
-          disabled={disableForm}
-          errorMessage={touched.cvv && errors.cvv}
-          helpMessage={cvvHelpMessage}
-        >
-          {props => (
-            <InputField
-              as={NumberInput}
-              {...props}
-              name="cvv"
-              value={values.cvv}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              maxLength={cvvLength}
-              autoComplete="cc-csc"
-            />
-          )}
-        </FieldContainer>
-        <div className="registred-card-cvv-form__buttons">
-          <Button type="submit" size="small" disabled={disableForm}>
-            Payer&nbsp;
-            <Price noColor price={totalPrice} />
-          </Button>
-          <Button type="reset" size="small" onClick={this.onCancel} disabled={disableForm}>
-            Annuler
-          </Button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <Form
+      decorators={[focusOnErrors]}
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form className="registred-card-cvv-form" onSubmit={handleSubmit}>
+          <InternalRegistredCardCvvForm totalPrice={totalPrice} brand={brand} disabled={disabled} onCancel={onCancel} />
+        </form>
+      )}
+    ></Form>
+  );
 }
 
-export default withFormik({
-  mapPropsToValues: () => ({
-    cvv: ""
-  }),
-  validateOnMount: true,
-  validate: (values, props) => {
-    const { brand } = props;
+RegistredCardCvvForm.propTypes = {
+  brand: PropTypes.string.isRequired,
+  totalPrice: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired
+  }).isRequired,
+  disabled: PropTypes.bool,
+  onCancel: PropTypes.func.isRequired,
+  onUseCard: PropTypes.func.isRequired
+};
 
-    const errors = {};
-
-    const cardBrandInfo = getTypeInfo(brand);
-
-    if (values.cvv) {
-      const cvvLength = cardBrandInfo && cardBrandInfo.code && cardBrandInfo.code.size;
-      if (cvvLength && cvvLength !== values.cvv.length) {
-        errors.cvv = `Le code de sécurité doit faire ${cvvLength} chiffres`;
-      }
-    } else {
-      if (brand !== "maestro") {
-        errors.cvv = "Le code de sécurité est requis";
-      }
+function InternalRegistredCardCvvForm({ brand, totalPrice, disabled, onCancel }) {
+  const { submitting } = useFormState({
+    subscription: {
+      submitting: true
     }
+  });
 
-    return errors;
-  },
-  handleSubmit: async (values, { props, setSubmitting }) => {
-    try {
-      await props.onUseCard(values);
-    } finally {
-      setSubmitting(false);
-    }
+  const { reset, resetFieldState } = useForm();
+
+  const cvv = useField("cvv", {
+    validate: value => {
+      const cardBrandInfo = getTypeInfo(brand);
+      if (value) {
+        const cvvLength = cardBrandInfo && cardBrandInfo.code && cardBrandInfo.code.size;
+        if (cvvLength && cvvLength !== value.length) {
+          return `Le code de sécurité doit faire ${cvvLength} chiffres`;
+        }
+      } else {
+        if (brand !== "maestro") {
+          return "Le code de sécurité est requis";
+        }
+      }
+    },
+    validateFields: []
+  });
+
+  const disableForm = submitting || disabled;
+
+  const cardBrandInfo = getTypeInfo(brand);
+  const cvvLength = cardBrandInfo.code.size;
+  const isMaestro = brand === "maestro";
+  const cvvHelpMessage = isMaestro
+    ? "Certaines cartes Maestro ne possèdent pas de code de sécurité. Si aucun code n'est présent, ne renseignez pas ce champ"
+    : null;
+
+  const onCancelClick = () => {
+    resetFieldState("cvv");
+    reset();
+    onCancel();
+  };
+
+  return (
+    <>
+      <FieldContainer
+        label="Code de sécurité"
+        className="registred-card-cvv-form__cvv"
+        disabled={disableForm}
+        errorMessage={getFieldError(cvv)}
+        helpMessage={cvvHelpMessage}
+        optional={isMaestro}
+      >
+        {props => (
+          <InputField
+            as={NumberInput}
+            {...cvv.input}
+            {...props}
+            name="cvv"
+            autoComplete="cc-csc"
+            maxLength={cvvLength}
+          />
+        )}
+      </FieldContainer>
+      <div className="registred-card-cvv-form__buttons">
+        <Button type="submit" size="small" disabled={disableForm}>
+          Payer&nbsp;
+          <Price noColor price={totalPrice} />
+        </Button>
+        <Button type="reset" size="small" onClick={onCancelClick} disabled={disableForm}>
+          Annuler
+        </Button>
+      </div>
+    </>
+  );
+}
+
+InternalRegistredCardCvvForm.propTypes = {
+  brand: PropTypes.string.isRequired,
+  totalPrice: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired
+  }).isRequired,
+  disabled: PropTypes.bool,
+  onCancel: PropTypes.func.isRequired
+};
+
+function getFieldError(field) {
+  const { touched, error, submitError, pristine } = field.meta;
+  if (touched && error) {
+    return error;
   }
-})(RegistredCardCvvForm);
+  if (submitError && pristine) {
+    return submitError;
+  }
+}
