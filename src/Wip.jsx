4c047fae24227a1ab6319b78401001c201080d5d
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-03-05 20:39:48 +0100

    Emotion: object style

--- src/Wip.jsx
+++ src/Wip.jsx
@@ -2,27 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { css } from "emotion/macro";
 
-function obliqueStripes(color) {
-  return css`
-    transition: background-color 0.3s ease-in;
-    background: repeating-linear-gradient(45deg, transparent, transparent 10px, black 10px, black 20px);
-    background-color: ${color};
-  `;
-}
-
-const wipClass = css`
-  ${obliqueStripes("yellow")}
-  padding: 10px;
-
-  &:hover {
-    ${obliqueStripes("orange")}
+const wipClass = css({
+  padding: "10px",
+  transition: "background-color 0.3s ease-in",
+  background: "repeating-linear-gradient(45deg, transparent, transparent 10px, black 10px, black 20px)",
+  backgroundColor: "yellow",
+  "&:hover": {
+    backgroundColor: "orange"
   }
-`;
+});
 
-const wipContainerClass = css`
-  padding: 10px;
-  background-color: white;
-`;
+const wipContainerClass = css({
+  padding: "10px",
+  backgroundColor: "white"
+});
 
 export default function Wip({ children }) {
   return (
