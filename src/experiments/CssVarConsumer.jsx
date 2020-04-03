4bcb3925d63ac0a9a7bc73b22e1e8800efb4942a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-04-03 10:33:54 +0200

    CssVarConsumer component

---
+++ src/experiments/CssVarConsumer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { css } from "emotion";
+
+const marginSize = "0.5rem";
+
+export default function CssVarConsumer({ customColor }) {
+  return (
+    <div
+      className={css({
+        ["--customColor"]: customColor,
+        border: "3px solid var(--customColor)",
+        padding: marginSize,
+      })}
+    >
+      <p
+        className={css({
+          margin: 0,
+          padding: 0,
+          backgroundColor: "white",
+          color: "black",
+        })}
+      >
+        C&apos;est formidable, les variables CSS.
+      </p>
+      <div
+        className={css({
+          marginTop: marginSize,
+          height: `calc(${marginSize} * 4)`,
+          backgroundColor: "var(--customColor)",
+        })}
+      />
+    </div>
+  );
+}
+
+CssVarConsumer.propTypes = {
+  customColor: PropTypes.string.isRequired,
+};
---
+++ src/experiments/CssVarConsumer.stories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { withKnobs, text } from "@storybook/addon-knobs";
+
+import CssVarConsumer from "./CssVarConsumer";
+
+export default {
+  title: "Experiments | CssVarConsumer",
+  component: CssVarConsumer,
+  decorators: [withKnobs],
+};
+
+export const main = () => <CssVarConsumer customColor={text("customColor", "red")} />;
