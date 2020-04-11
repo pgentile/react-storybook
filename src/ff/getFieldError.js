09c7721522a01805bb15ed0cd028ddb3260b1ff7
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-04-11 09:46:11 +0200

    Dead code elimination

--- src/ff/getFieldError.js
+++
@@ -1,9 +0,0 @@
-export default function getFieldError(field) {
-  const { touched, error, submitError, pristine } = field.meta;
-  if (touched && error) {
-    return error;
-  }
-  if (submitError && pristine) {
-    return submitError;
-  }
-}
