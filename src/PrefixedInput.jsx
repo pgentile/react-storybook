9d1b96e3ce83b1daeebfadb4a5c9eb38f450c6c6
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-10-23 08:32:45 +0200

    Prefixed input

---
+++ src/PrefixedInput.jsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useRef } from "react";
+import PropTypes from "prop-types";
+
+import "./PrefixedInput.scss";
+
+export default function PrefixedInput({ prefix, ...other }) {
+  const inputRef = useRef();
+
+  const handlePrefixClick = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <span className="prefixed-input">
+      {prefix && (
+        <span className="prefixed-input__prefix" onClick={handlePrefixClick}>
+          {prefix}
+        </span>
+      )}
+      <input className="prefixed-input__input" {...other} ref={inputRef} />
+    </span>
+  );
+}
+
+PrefixedInput.propTypes = {
+  prefix: PropTypes.node.isRequired,
+};
---
+++ src/PrefixedInput.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PrefixedInput from "./PrefixedInput";
+
+export default {
+  title: "PrefixedInput",
+  component: PrefixedInput,
+};
+
+export const main = () => <PrefixedInput prefix="1180&nbsp;" />;
