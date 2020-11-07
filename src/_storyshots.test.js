1e152ef8b42fe1e7a43249e3bc6bb425a861bdd9
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-11-07 11:05:00 +0100

    Use mock instead of incremented HTML ID

--- src/_storyshots.test.js
+++ src/_storyshots.test.js
@@ -1,5 +1,7 @@
 import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
 
+jest.mock("./forms/useGeneratedFieldId.js");
+
 initStoryshots({
   integrityOptions: {
     cwd: __dirname,
---
+++ src/forms/__mocks__/useGeneratedFieldId.js
@@ -0,0 +1,3 @@
+export default function useGeneratedFieldId(id) {
+  return id || "__mock_generated_id__";
+}
