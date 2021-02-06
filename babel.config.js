dd235398559050608ccd14e8e94f40c1d3cae5c3
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-02-06 18:07:16 +0100

    Suppression packages fournis dans preset env

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
 module.exports = (api) => {
   return {
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-private-methods",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "@babel/plugin-proposal-optional-chaining",
       "macros",
       [
         "formatjs",
